72cd942cc8d473ba86ac06ca73dafa2fb8ab618b
Gabriel Kind <gkind@hs-mittweida.de>
2020-03-06 11:29:48 +0100

    Remove .d.ts files

--- wedesign/static/wedesign/js/datatable_reactions.d.ts
+++
@@ -1,6 +0,0 @@
-import "datatables.net";
-export declare class Table {
-    private tbl;
-    private buttons;
-    constructor(where: HTMLElement);
-}
--- wedesign/static/wedesign/js/design_utils.d.ts
+++
@@ -1,8 +0,0 @@
-export declare namespace DesignUtils {
-    function downloadSVG(svg_element: Node, filename: string): void;
-    function downloadText(text: string, filename: string): void;
-    function downloadCSV(csv: string, filename: string): void;
-    function downloadJson(json: string, filename: string): void;
-    function downloadURI(uri: string, name: string): void;
-    function downloadPNG(svg_element: Node, filename: string): void;
-}
